65bb8350a694511f10207305c51277f81f9f0a77
Anurag-Band <anuragband10@gmail.com>
2025-01-06 19:28:31 +0530

    second pattern code

--- src/Patterns.java
+++ src/Patterns.java
@@ -1,12 +1,20 @@
 public class Patterns {
     public static void main(String[] args) {
-//  First Pattern :- square with zero's
+//  Pattern 1 :- square with zero's
 
-        for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j <= 5; j++) {
-                System.out.print(" 0 ");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i <= 4; i++) {
+//            for (int j = 0; j <= 5; j++) {
+//                System.out.print(" 0 ");
+//            }
+//            System.out.println();
+//        }
+//  Pattern 2  :-
+
+//        for (int i = 0; i<=4; i++){
+//            for (int j = 0; j < i + 1; j++){
+//                System.out.print("0");
+//            }
+//            System.out.println();
+//        }
     }
 }
