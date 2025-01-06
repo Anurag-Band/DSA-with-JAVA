65b1ea9b36d0ba741936e89f642e187e09d3377e
Anurag-Band <anuragband10@gmail.com>
2025-01-06 17:03:09 +0530

    first pattern code

---
+++ src/Patterns.java
@@ -0,0 +1,12 @@
+public class Patterns {
+    public static void main(String[] args) {
+//  First Pattern :- square with zero's
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 6; j++) {
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
