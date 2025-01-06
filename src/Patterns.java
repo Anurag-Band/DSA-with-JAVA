c3d54d0cbc1414f44d09936d8a6f296512f10b91
Anurag-Band <anuragband10@gmail.com>
2025-01-06 19:24:12 +0530

    changes first pattern code

--- src/Patterns.java
+++ src/Patterns.java
@@ -3,8 +3,8 @@ public class Patterns {
 //  First Pattern :- square with zero's
 
         for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j <= 6; j++) {
-                System.out.print("0");
+            for (int j = 0; j <= 5; j++) {
+                System.out.print(" 0 ");
             }
             System.out.println();
         }
