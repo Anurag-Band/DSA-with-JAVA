2b4c591edddcdaba73a0f5dcba2a427f5bf8cd23
Anurag-Band <anuragband10@gmail.com>
2025-01-06 19:45:43 +0530

    third pattern code

--- src/Patterns.java
+++ src/Patterns.java
@@ -8,11 +8,23 @@ public class Patterns {
 //            }
 //            System.out.println();
 //        }
+
 //  Pattern 2  :-
 
 //        for (int i = 0; i<=4; i++){
 //            for (int j = 0; j < i + 1; j++){
 //                System.out.print("0");
+//            }
+//            System.out.println();
+//        }
+
+//  Pattern 3  :-
+
+//        for (int i = 0; i<=3; i++){
+//            int num = 0;
+//            for (int j = 0; j < i + 1; j++){
+//                num++;
+//                System.out.print(num);
 //            }
 //            System.out.println();
 //        }
