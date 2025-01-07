69d01abeff4efa6df1e608d4f12d530eef04f3a4
Anurag-Band <anuragband10@gmail.com>
2025-01-07 19:13:08 +0530

    lecture 5 : question 2 output

--- src/Patterns.java
+++ src/Patterns.java
@@ -214,5 +214,45 @@ public class Patterns {
 //            System.out.println();
 //        }
 
+
+//  Lecture 5 :- Q.2 ->>
+//        for n = 5 (Note: n is always a Odd number...)
+//        print :-
+//                     . . *
+//                     . * * *
+//                     * * * * *
+//                     . * * *
+//                     . . *
+
+//        System.out.print("Enter the value of n : ");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int x = (n / 2) + 1;
+//
+//        // First part
+//        for (int i = 0; i < x; i++) {
+//            for (int j = 1; j <= x- i - 1; j++) {
+//                System.out.print(" . ");
+//            }
+//            for (int j = 1; j <= 2 * i + 1; j++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+//
+//        // Second part
+//        for (int i = 1; i <= (n - x); i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(" . ");
+//            }
+//            for (int j = 1; j <= n - (2 * i); j++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+
+
+
     }
 }
