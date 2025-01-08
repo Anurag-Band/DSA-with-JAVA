3865478297fbb7cafe82bc00c0b005782d8ee176
Anurag-Band <anuragband10@gmail.com>
2025-01-08 10:10:12 +0530

    Sum of pairs using Two  arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -115,6 +115,22 @@ public class Arrays {
 //
 //         System.out.print("Sum : " + sum);
 
+//        ---------------------------------------------
+
+//        Q. 4) Sum of pairs using Two arrays
+
+//         int nums1[] = {1,2,3};
+//         int nums2[] = {4,5,6};
+//
+//         int sum = 0;
+//
+//         for (int i = 0; i < nums1.length; i++){
+//             for (int j = 0; j < nums2.length; j++){
+//                 sum += nums1[i] + nums2[j];
+//             }
+//         }
+//
+//        System.out.print("Sum : " + sum);
 
 
     }
