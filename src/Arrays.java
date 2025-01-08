7d7c9f030f22a3c1f5308fc97dfa4591026d90d6
Anurag-Band <anuragband10@gmail.com>
2025-01-08 11:04:39 +0530

    Sum of Triplets

--- src/Arrays.java
+++ src/Arrays.java
@@ -154,6 +154,22 @@ public class Arrays {
 //
 //        System.out.print("Max Sum : " + maxSum);
 
+//        ---------------------------------------------
+
+//        Q. 6) find Sum of Triplets
+
+//         int nums[] = {1,2,3,4};
+//         int sum = 0;
+//
+//         for (int i = 0; i < nums.length - 2; i++){
+//             for (int j = i + 1; j < nums.length - 1; j++){
+//                 for (int k = j + 1; k < nums.length; k++){
+//                     sum = nums[i] + nums[j] + nums[k];
+//                     System.out.println("Sum : " + sum);
+//                 }
+//             }
+//         }
+
 
 
     }
